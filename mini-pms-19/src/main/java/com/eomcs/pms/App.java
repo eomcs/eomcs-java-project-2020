4861be1e2b4ba87cdd98fbf6072339b04842437e
Jinyoung Eom <jinyoung.eom@gmail.com>
2020-08-29 22:32:20 +0900

    v19

---
+++ mini-pms-19/src/main/java/com/eomcs/pms/App.java
@@ -0,0 +1,55 @@
+package com.eomcs.pms;
+
+import com.eomcs.pms.handler.BoardHandler;
+import com.eomcs.pms.handler.MemberHandler;
+import com.eomcs.pms.handler.ProjectHandler;
+import com.eomcs.pms.handler.TaskHandler;
+import com.eomcs.util.Prompt;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    BoardHandler boardHandler = new BoardHandler();
+    MemberHandler memberHandler = new MemberHandler();
+    ProjectHandler projectHandler = new ProjectHandler(memberHandler);
+    TaskHandler taskHandler = new TaskHandler(memberHandler);
+
+    loop:
+      while (true) {
+        String command = Prompt.inputString("명령> ");
+
+        switch (command) {
+          case "/member/add": memberHandler.add(); break;
+          case "/member/list": memberHandler.list(); break;
+          case "/member/detail": memberHandler.detail(); break;
+          case "/member/update": memberHandler.update(); break;
+          case "/member/delete": memberHandler.delete(); break;
+          case "/project/add": projectHandler.add(); break;
+          case "/project/list": projectHandler.list(); break;
+          case "/project/detail": projectHandler.detail(); break;
+          case "/project/update": projectHandler.update(); break;
+          case "/project/delete": projectHandler.delete(); break;
+          case "/task/add": taskHandler.add(); break;
+          case "/task/list": taskHandler.list(); break;
+          case "/task/detail": taskHandler.detail(); break;
+          case "/task/update": taskHandler.update(); break;
+          case "/task/delete": taskHandler.delete(); break;
+          case "/board/add": boardHandler.add(); break;
+          case "/board/list": boardHandler.list(); break;
+          case "/board/detail": boardHandler.detail(); break;
+          case "/board/update": boardHandler.update(); break;
+          case "/board/delete": boardHandler.delete(); break;
+          case "quit":
+          case "exit":
+            System.out.println("안녕!");
+            break loop;
+          default:
+            System.out.println("실행할 수 없는 명령입니다.");
+        }
+        System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
+      }
+
+    Prompt.close();
+  }
+}
