23b91262b94681f5808497d28cdf7807e9e0bec3
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-24 12:13:36 +0900

    v31-b

---
+++ mini-pms-31-b/README.md
@@ -0,0 +1,33 @@
+# 31-b. 파일 입출력 API를 활용하여 데이터를 읽고 쓰기 II : BufferedReader/BufferedWriter
+
+이번 훈련에서는 **character stream class** 의 `BufferedReader/BufferedWriter` 를 활용하여 데이터 읽기, 쓰기 속도를 높이는 것을 연습할 것이다.
+
+**BufferedReader/BufferedWriter** 는,
+- 동작원리가 `BufferedInputStream/BufferedOutputStream` 와 같다.
+
+
+## 훈련 목표
+
+- `BufferedReader/BufferedWriter` 를 사용하는 이유와 동작 원리를 이해한다. 
+
+## 훈련 내용
+
+- `FileReader/FileWriter` 에 `BufferedReader/BufferedWriter` 데코레이터를 붙여 읽기, 쓰기 속도를 높인다.
+
+
+## 실습
+
+
+### 1단계 - FileReader/FileWriter 에 BufferedReader/BufferedWriter 데코레이터를 연결한다.
+
+- App 클래스
+  - 기존의 `saveXxx()` 와 `loadXxx()`를 변경한다.
+
+#### 작업 파일
+
+- com.eomcs.pms.App 변경
+
+
+## 실습 결과
+
+- src/main/java/com/eomcs/pms/App.java 변경
---
+++ mini-pms-31-b/src/main/java/com/eomcs/pms/handler/TaskDetailCommand.java
@@ -0,0 +1,52 @@
+package com.eomcs.pms.handler;
+
+import java.util.List;
+import com.eomcs.pms.domain.Task;
+import com.eomcs.util.Prompt;
+
+public class TaskDetailCommand implements Command {
+
+  List<Task> taskList;
+
+  public TaskDetailCommand(List<Task> list) {
+    this.taskList = list;
+  }
+
+  @Override
+  public void execute() {
+    System.out.println("[작업 상세보기]");
+    int no = Prompt.inputInt("번호? ");
+    Task task = findByNo(no);
+
+    if (task == null) {
+      System.out.println("해당 번호의 작업이 없습니다.");
+      return;
+    }
+
+    System.out.printf("내용: %s\n", task.getContent());
+    System.out.printf("마감일: %s\n", task.getDeadline());
+    String stateLabel = null;
+    switch (task.getStatus()) {
+      case 1:
+        stateLabel = "진행중";
+        break;
+      case 2:
+        stateLabel = "완료";
+        break;
+      default:
+        stateLabel = "신규";
+    }
+    System.out.printf("상태: %s\n", stateLabel);
+    System.out.printf("담당자: %s\n", task.getOwner());
+  }
+
+  private Task findByNo(int no) {
+    for (int i = 0; i < taskList.size(); i++) {
+      Task task = taskList.get(i);
+      if (task.getNo() == no) {
+        return task;
+      }
+    }
+    return null;
+  }
+}
