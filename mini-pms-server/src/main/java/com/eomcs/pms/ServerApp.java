96e93a30623180fffbf81194cb22460e15ead251
eomjinyoung <jinyoung.eom@gmail.com>
2020-10-20 11:18:01 +0900

    v36-a

--- mini-pms-server/src/main/java/com/eomcs/pms/ServerApp.java
+++ mini-pms-server/src/main/java/com/eomcs/pms/ServerApp.java
@@ -10,14 +10,12 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import com.eomcs.context.ApplicationContextListener;
 import com.eomcs.pms.handler.Command;
 import com.eomcs.pms.listener.AppInitListener;
 import com.eomcs.pms.listener.DataHandlerListener;
 import com.eomcs.pms.listener.RequestMappingListener;
+import com.eomcs.util.concurrent.ThreadPool;
 
 public class ServerApp {
 
@@ -26,7 +24,7 @@ public class ServerApp {
   static boolean stop = false;
 
   // 스레드풀 준비
-  ExecutorService threadPool = Executors.newCachedThreadPool();
+  ThreadPool threadPool = new ThreadPool();
 
   // 옵저버와 공유할 맵 객체
   static Map<String,Object> context = new Hashtable<>();
@@ -71,7 +69,7 @@ public class ServerApp {
         if (stop) {
           break;
         }
-        // 직접 스레드를 생성하는 것이 아니라 스레드풀에 작업을 맡긴다.
+        // 람다 문법 사용
         threadPool.execute(() -> handleClient(clientSocket));
       }
 
@@ -80,31 +78,6 @@ public class ServerApp {
     }
 
     notifyApplicationContextListenerOnServiceStopped();
-
-    // 스레드풀을 종료한다.
-    threadPool.shutdown();
-
-    try {
-      // 스레드풀의 모든 스레드가 종료될 때까지 기다린다.
-      if (!threadPool.awaitTermination(10, TimeUnit.SECONDS)) {
-        System.out.println("아직 종료 안된 작업이 있다.");
-        System.out.println("남아 있는 작업의 강제 종료를 시도하겠다.");
-        // => 만약 10초가 경과될 때까지 종료되지 않으면,
-        //    수행 중인 작업을 강제 종료하라고 지시하고,
-        //    대기 중인 작업은 취소한다.
-        threadPool.shutdownNow();
-
-        // 그리고 다시 작업이 종료될 때까지 기다린다.
-        if (!threadPool.awaitTermination(5, TimeUnit.SECONDS)) {
-          System.out.println("스레드풀의 강제 종료를 완료하지 못했다.");
-        } else {
-          System.out.println("모든 작업을 강제 종료했다.");
-        }
-      }
-    } catch (Exception e) {
-      System.out.println("스레드풀 종료 중 오류 발생!");
-    }
-    System.out.println("서버 종료!");
   }
 
   public static void main(String[] args) {
@@ -148,6 +121,7 @@ public class ServerApp {
       // 응답의 끝을 알리는 빈 문자열을 보낸다.
       out.println();
       out.flush();
+
 
     } catch (Exception e) {
       System.out.println("클라이언트와의 통신 오류!");
---
+++ mini-pms-server/src/main/java/com/eomcs/util/concurrent/ThreadPool.java
@@ -0,0 +1,107 @@
+package com.eomcs.util.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadPool {
+
+  List<Worker> workers = new ArrayList<>();
+
+  // 스레드 + 작업을 맡기고 + 깨우는 기능 + 작업 완료 후 잠자는 기능
+  class Worker extends Thread {
+
+    Runnable task;
+
+    public void setTask(Runnable task) {
+      // 스레드가 할 작업을 배정한 후
+      this.task = task;
+
+      synchronized (this) {
+        // 스레드를 깨운다.
+        this.notify();
+      }
+    }
+
+    @Override
+    public void run() {
+      synchronized (this) {
+        while (true) {
+          try {
+            System.out.printf("[%s] - 스레드 대기 중...\n", this.getName());
+
+            this.wait();
+
+            System.out.printf("[%s] - 스레드 작업 시작!\n", this.getName());
+            // 이 스레드 객체에 대해 대기 상태로 있다가
+            // 이 스레드에게 깨어나라는 알림(notify()/nofifyAll())이 온다면
+            // 즉시 running 상태로 되돌아 간다.
+
+          } catch (Exception e) {
+            System.out.printf("[%s] - 스레드 실행 중 오류 발생!\n", this.getName());
+            break;
+            // 기다리다가 인터럽트 예외가 발생하면
+            // 스레드를 종료한다.
+          }
+          try {
+            task.run();
+            System.out.printf("[%s] - 스레드 작업 종료!\n", this.getName());
+
+          } catch (Exception e) {
+            System.out.printf("[%s] - %s\n",
+                this.getName(), e.getMessage());
+            // 작업 수행 중 오류가 발생하더라도
+            // 스레드는 계속 유효하다.
+          } finally {
+            // 정상적인 종료든 작업 수행 중 예외가 발생했든 간에
+            // 작업을 마친 스레드는 재사용할 수 있도록
+            // 다시 목록에 보관되어야 한다.
+            workers.add(this);
+            System.out.printf("[%s] - 스레드풀로 되돌아 감!\n", this.getName());
+          }
+        }
+      }
+    }
+  }
+
+  public void execute(Runnable task) {
+    Worker t;
+    if (workers.size() == 0) {
+      // 스레드가 없다면 새로 생성한다.
+      t = new Worker();
+      System.out.printf("[%s] - 스레드 생성!\n", t.getName());
+
+      // 그리고 즉시 실행한다.
+      // - 실행하더라도 스레드는 처음에 스스로 대기 상태로 갈 것이다.
+      t.start();
+
+      // 현재 main 스레드를 잠깐 멈추게 하여
+      // 새로 만든 스레드가 실행할 틈을 주자!
+      // 그래야만 새로 만든 스레드가 실행하자 마자 대기상태로 간다.
+      try {
+        Thread.sleep(20);
+      } catch (Exception e) {
+        // sleep() 중 발생한 예외는 무시한다.
+      }
+    } else {
+      // 스레드가 있다면, 스레드풀에서 한 개 꺼낸다.
+      // - 스레드풀에서 꺼낸 스레드는 현재 대기 상태이다.
+      t = workers.remove(0);
+      System.out.printf("[%s] - 스레드 꺼내서 재사용!\n", t.getName());
+    }
+
+    // 스레드를 깨워서 일을 시킨다.
+    // - 스레드에게 해야 할 작업을 배정하면 된다.
+    t.setTask(task);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
