952650d3d4b0f90745bcb120ccc705c5ef21fa85
eomjinyoung <jinyoung.eom@gmail.com>
2020-11-02 14:43:58 +0900

    v37-c

--- mini-pms-client/src/main/java/com/eomcs/pms/handler/ProjectListCommand.java
+++ mini-pms-client/src/main/java/com/eomcs/pms/handler/ProjectListCommand.java
@@ -20,13 +20,31 @@ public class ProjectListCommand implements Command {
 
       try (ResultSet rs = stmt.executeQuery()) {
         System.out.println("번호, 프로젝트명, 시작일 ~ 종료일, 관리자, 팀원");
+
         while (rs.next()) {
-          System.out.printf("%d, %s, %s ~ %s, %s, []\n",
+          StringBuilder members = new StringBuilder();
+          try (PreparedStatement stmt2 = con.prepareStatement(
+              "select mp.member_no, m.name"
+                  + " from pms_member_project mp"
+                  + " inner join pms_member m on mp.member_no=m.no"
+                  + " where mp.project_no=" + rs.getInt("no"));
+              ResultSet memberRs = stmt2.executeQuery()) {
+
+            while (memberRs.next()) {
+              if (members.length() > 0) {
+                members.append(",");
+              }
+              members.append(memberRs.getString("name"));
+            }
+          }
+
+          System.out.printf("%d, %s, %s ~ %s, %s, [%s]\n",
               rs.getInt("no"),
               rs.getString("title"),
               rs.getString("sdt"),
               rs.getString("edt"),
-              rs.getString("owner_name"));
+              rs.getString("owner_name"),
+              members.toString());
         }
       }
     } catch (Exception e) {
