b6f746894e90d542b5ffbe5d55fadc15fca83516
eomjinyoung <jinyoung.eom@gmail.com>
2020-11-27 11:57:42 +0900

    v44

--- mini-pms-server/src/main/java/com/eomcs/pms/web/BoardAddServlet.java
+++ mini-pms-server/src/main/java/com/eomcs/pms/web/BoardAddServlet.java
@@ -42,7 +42,7 @@ public class BoardAddServlet extends HttpServlet {
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
-    out.println("<meta http-equiv='Refresh' content='1;list'>");
+    out.println("<meta http-equiv='Refresh' content='1;url=list'>");
     out.println("<title>게시글등록</title></head>");
     out.println("<body>");
     try {
